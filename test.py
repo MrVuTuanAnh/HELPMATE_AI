783a91278dc608f74e7036ff3610ba20000dd310
alexander jessi <info@cybervietnam.net>
2024-04-10 10:03:44 +0700

    Update all files

--- test.py
+++
@@ -1,6 +0,0 @@
-import chromadb
-
-# Hypothetical incorrect usage
-settings = {"some_setting": "value"}
-additional_params = {"settings": settings}
-chroma_client = chromadb.Client("chromadb_index", settings=settings, **additional_params)
