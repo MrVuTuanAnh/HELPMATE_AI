e711bd6e8b5ff09c94749ea129f467a27056bc41
Cyber Vietnam Inc <anhvt@tpd.com.vn>
2024-04-12 01:08:19 +0700

    Update all files

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-<<<<<<< HEAD
-=======
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
+import openai
 from pydantic import BaseModel
 import uvicorn
 import os
@@ -14,9 +13,10 @@ import pandas as pd
 from dotenv import load_dotenv
 import fitz  # PyMuPDF
 import numpy as np
-from sentence_transformers import SentenceTransformer
+from sentence_transformers import CrossEncoder, SentenceTransformer
 from chromadb import PersistentClient
-from chromadb.utils.embedding_functions import SentenceTransformerEmbeddingFunction
+from chromadb.utils.embedding_functions import OpenAIEmbeddingFunction, SentenceTransformerEmbeddingFunction
+
 
 # Load environment variables
 load_dotenv()
@@ -34,8 +34,26 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+##################
+# GLOBAL VARIABLES
+
+client = PersistentClient(path='./chromadb/')  # ChromaDB Integration for Embeddings
+model = "text-embedding-ada-002"
+embedding_function = OpenAIEmbeddingFunction(api_key=OPENAI_API_KEY, model_name=model)
+sentence_transformer_ef = SentenceTransformerEmbeddingFunction(model_name="all-MiniLM-L6-v2")
+cache_collection = client.get_or_create_collection(name='Insurance_Cache', embedding_function=embedding_function)
+cross_encoder = CrossEncoder('cross-encoder/ms-marco-MiniLM-L-6-v2')
+
+#
+#
+#
 # Utility Functions for processing
 def download_pdf(url, save_path):
+    print("=" * 20)
+    print("==== check_bboxes ====")
+    print("=" * 20)
+    print()
+
     """Downloads a PDF from a URL and saves it to the specified path."""
     try:
         response = requests.get(url)
@@ -47,12 +65,22 @@ def download_pdf(url, save_path):
         print(f"Failed to download file: {e}")
 
 def check_bboxes(word, table_bbox):
+    print("=" * 20)
+    print("==== check_bboxes ====")
+    print("=" * 20)
+    print()
+        
     """Checks if a word's bounding box is within a table's bounding box."""
     l = word['x0'], word['top'], word['x1'], word['bottom']
     r = table_bbox
     return l[0] > r[0] and l[1] > r[1] and l[2] < r[2] and l[3] < r[3]
 
 def extract_text_from_pdf(pdf_path):
+    print("=" * 20)
+    print("==== Extracting Text from PDF ====")
+    print("=" * 20)
+    print()
+
     """Extracts and clusters text from a PDF, distinguishing between table and non-table content."""
     p = 0
     full_text = []
@@ -67,121 +95,127 @@ def extract_text_from_pdf(pdf_path):
             p += 1
     return full_text
 
-# PDF Processing and Data Preparation
-url = "https://cdn.upgrad.com/uploads/production/585ca56a-6fe1-4b93-903c-1c1a1de74bf1/Principal-Sample-Life-Insurance-Policy.pdf"
-save_path = './data/Principal-Sample-Life-Insurance-Policy.pdf'
-download_pdf(url, save_path)
 
-data = [pd.DataFrame(extract_text_from_pdf(pdf_path), columns=['Page No.', 'Page_Text']).assign(**{'Document Name': pdf_path.name})
-        for pdf_path in Path("./data/").glob("*.pdf")]
 
-# Data Manipulation and Analysis
-if data:
+def main():
+    # PDF Processing and Data Preparation
+    url = "https://cdn.upgrad.com/uploads/production/585ca56a-6fe1-4b93-903c-1c1a1de74bf1/Principal-Sample-Life-Insurance-Policy.pdf"
+    save_path = './data/Principal-Sample-Life-Insurance-Policy.pdf'
+    download_pdf(url, save_path)
+
+    data = [
+        pd.DataFrame(extract_text_from_pdf(pdf_path), columns=['Page No.', 'Page_Text']).assign(**{'Document Name': pdf_path.name})
+        for pdf_path in Path("./data/").glob("*.pdf")
+    ]
+
+    if not data:
+        raise ValueError("No PDFs found in the data directory.")
+    
+    # Data Manipulation and Analysis
     insurance_pdfs_data = pd.concat(data, ignore_index=True).loc[lambda df: df['Page_Text'].str.split().str.len() >= 10]
     insurance_pdfs_data['Metadata'] = insurance_pdfs_data.apply(lambda row: {'Policy_Name': row['Document Name'][:-4], 'Page_No.': row['Page No.']}, axis=1)
-    print(insurance_pdfs_data.head())
 
-# ChromaDB Integration for Embeddings
-client = PersistentClient(path='./chromadb/')
-embedding_function = SentenceTransformerEmbeddingFunction(model_name="all-MiniLM-L6-v2")
-insurance_collection = client.get_or_create_collection(name='RAG_on_Insurance', embedding_function=embedding_function)
-insurance_collection.add(documents=insurance_pdfs_data["Page_Text"].tolist(), ids=[str(i) for i in range(len(insurance_pdfs_data))], 
-                         metadatas=insurance_pdfs_data['Metadata'].tolist())
+    # ChromaDB Integration for Embeddings
+    insurance_collection = client.get_or_create_collection(name='RAG_on_Insurance', embedding_function=sentence_transformer_ef)
+    insurance_collection.add(
+        documents=insurance_pdfs_data["Page_Text"].tolist(), 
+        ids=[str(i) for i in range(len(insurance_pdfs_data))], 
+        metadatas=insurance_pdfs_data['Metadata'].tolist()
+    )
 
+    def exec_query_search(query):
+        if not query:
+            raise ValueError("No query provided.")
 
-# Read the user query
-query = input()
+        cache_results = cache_collection.query(query_texts=query, n_results=1)
+        print("Raw cache results: ", cache_results)
+        
+        results = insurance_collection.query(query_texts=query, n_results=10)
+        print("Raw results: ", results.items())
+        
+        threshold = 0.2
+        
+        ids = []
+        documents = []
+        distances = []
+        metadatas = []
+        results_df = pd.DataFrame()
+        
+        if cache_results['distances'][0] == [] or cache_results['distances'][0][0] > threshold:
+            results = insurance_collection.query(query_texts=query, n_results=10)
+        
+            keys = []
+            values = []
+            
+            for _key, _val in results.items():
+                if _val is None:
+                    continue
+                
+                if _key != 'embeddings':
+                    for i in range(10):
+                        keys.append(str(_key) + str(i))
+                        values.append(str(_val[0][i]))
+            
+            cache_collection.add(
+                documents=[query],
+                ids=[query],
+                metadatas=dict(zip(keys, values))
+            )
+        
+            print("Not found in cache. Found in main collection.")
+            result_dict = {'Metadatas': results['metadatas'][0], 'Documents': results['documents'][0], 'Distances': results['distances'][0], "IDs":results["ids"][0]}
+            results_df = pd.DataFrame.from_dict(result_dict)
+            print(f"Not found cache: \n {results_df}")
+        elif cache_results['distances'][0][0] <= threshold:
+            cache_result_dict = cache_results['metadatas'][0][0]
+            
+            for key, value in cache_result_dict.items():
+                if 'ids' in key:
+                    ids.append(value)
+                elif 'documents' in key:
+                    documents.append(value)
+                elif 'distances' in key:
+                    distances.append(value)
+                elif 'metadatas' in key:
+                    metadatas.append(value)
+            
+            print("Found in cache!")
+            results_df = pd.DataFrame({
+                'IDs': ids,
+                'Documents': documents,
+                'Distances': distances,
+                'Metadatas': metadatas
+            })
+            
+            print(f"Found cache: \n {results_df}")
+        return results_df
 
-# Searh the Cache collection first
-# Query the collection against the user query and return the top 20 results
-cache_results = cache_collection.query(
-    query_texts=query,
-    n_results=1
-)
-cache_results
+    query = input("Enter your query: (What are the default benefits and provisions of the Group Policy?)")
+    results_df = exec_query_search(query)
 
-results = insurance_collection.query(
-query_texts=query,
-n_results=10
-)
+    query2 = input("Enter your query: (What does it mean by 'the later of the Date of Issue'?)") 
+    results_df2 = exec_query_search(query2)
 
-# Implementing Cache in Semantic Search
-# Set a threshold for cache search
-threshold = 0.2
+    query3 = input("Enter your query: (What happens if a third-party service provider fails to provide the promised goods and services?)")
+    results_df3 = exec_query_search(query3)
 
-ids = []
-documents = []
-distances = []
-metadatas = []
-results_df = pd.DataFrame()
+    # Re-Ranking with cross encoder
+    # Test the cross encoder model
+    scores = cross_encoder.predict(
+        [
+            ['Does the insurance cover diabetic patients?', 'The insurance policy covers some pre-existing conditions including diabetes, heart diseases, etc. The policy does not howev'],
+            ['Does the insurance cover diabetic patients?', 'The premium rates for various age groups are given as follows. Age group (<18 years): Premium rate']
+        ]
+    )
+    
+    print(f'scores: {scores}')
+    
+    # .
+    # .
+    # .
+    # .
 
-# If the distance is greater than the threshold, then return the results from the main collection.
-if cache_results['distances'][0] == [] or cache_results['distances'][0][0] > threshold:
-      # Query the collection against the user query and return the top 10 results
-      results = insurance_collection.query(
-      query_texts=query,
-      n_results=10
-      )
+    
 
-      # Store the query in cache_collection as document w.r.t to ChromaDB so that it can be embedded and searched against later
-      # Store retrieved text, ids, distances and metadatas in cache_collection as metadatas, so that they can be fetched easily if a query indeed matches to a query in cache
-      Keys = []
-      Values = []
-
-      for key, val in results.items():
-        if val is None:
-          continue
-        if key != 'embeddings':
-          for i in range(10): # Top 10 variable, we can also put as 25 for top_n
-            Keys.append(str(key)+str(i))
-            Values.append(str(val[0][i]))
-
-      cache_collection.add(
-          documents= [query],
-          ids = [query],  # Or if you want to assign integers as IDs 0,1,2,.., then you can use "len(cache_results['documents'])" as will return the no. of queries currently in the cache and assign the next digit to the new query."
-          metadatas = dict(zip(Keys, Values))
-      )
-
-      print("Not found in cache. Found in main collection.")
-
-      result_dict = {'Metadatas': results['metadatas'][0], 'Documents': results['documents'][0], 'Distances': results['distances'][0], "IDs":results["ids"][0]}
-      results_df = pd.DataFrame.from_dict(result_dict)
-      results_df
-
-
-# If the distance is, however, less than the threshold, you can return the results from cache
-elif cache_results['distances'][0][0] <= threshold:
-      cache_result_dict = cache_results['metadatas'][0][0]
-
-      # Loop through each inner list and then through the dictionary
-      for key, value in cache_result_dict.items():
-          if 'ids' in key:
-              ids.append(value)
-          elif 'documents' in key:
-              documents.append(value)
-          elif 'distances' in key:
-              distances.append(value)
-          elif 'metadatas' in key:
-              metadatas.append(value)
-
-      print("Found in cache!")
-
-      # Create a DataFrame
-      results_df = pd.DataFrame({
-        'IDs': ids,
-        'Documents': documents,
-        'Distances': distances,
-        'Metadatas': metadatas
-      })
-
-results_df
-
-results.items()
-print(insurance_collection.get(ids=['0', '1', '2'], include=['embeddings', 'documents', 'metadatas']))
-
-# Retrieve or create the main collection where documents are stored
-main_collection = client.get_or_create_collection(name='RAG_on_Insurance', embedding_function=embedding_function)
-
-# Retrieve or create a cache collection to store recent queries and results
-cache_collection = client.get_or_create_collection(name='Insurance_Cache', embedding_function=embedding_function)
->>>>>>> bcb1c92e836089a280026c51523585bbac628738
+if __name__ == "__main__":
+    main()
